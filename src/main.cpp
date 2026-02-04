1648cd7cf4d5175c22ed8df5642bb900ddbba03c
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-02-04 13:11:25 +0100

    initial project

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <windows.h>
+#include <iostream>
+
+void Main()
+{
+    MessageBox(nullptr, "AltairFix DLL Loaded!", "Info", MB_OK);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
+{
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Code to run when the DLL is loaded into a process
+        DisableThreadLibraryCalls(hModule);
+        Main();
+        break;
+    case DLL_PROCESS_DETACH:
+        // Code to run when the DLL is unloaded from a process
+        break;
+    }
+    return TRUE;
+}
